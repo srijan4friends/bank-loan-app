55f82a30456dab17951f188565c8663c6fb27dfa
Srijan Khan <srijan@Srijans-MacBook-Air.local>
2020-10-12 11:27:33 -0500

    4th comit

--- bank-user-svc/src/models/user.js
+++ bank-user-svc/src/models/user.js
@@ -117,12 +117,6 @@ const userSchema = new mongoose.Schema({
     }
 })
 
-/*userSchema.virtual('tasks', {
-    ref: 'Task',
-    localField: '_id',
-    foreignField: 'owner'
-})*/
-
 userSchema.methods.toJSON = function () {
     const user = this
     const userObject = user.toObject()
